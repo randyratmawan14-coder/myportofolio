2ff1e4ce831bf2b266b6b8c798ac08d4c912ddc9
randyratmawan14-coder <randyratmawan14@gmail.com>
2026-02-07 20:14:52 +0700

    update layout

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@ const navLinks = document.getElementById('nav-links');
 toggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+const images = document.querySelectorAll(".lightbox")
+
+images.forEach((img) => {
+    img.addEventListener("click", () => {
+        if (document.querySelector(".lightbox-overlay")) 
+            return;
+
+        const overlay = document.createElement("div");
+        overlay.className = "lightbox-overlay";
+
+        const largeImg = document.createElement("img");
+        largeImg.src = img.src;
+
+        overlay.appendChild(largeImg);
+        document.body.appendChild(overlay);
+
+        overlay.addEventListener("click", (e) => {
+            if (e.target === overlay){
+                overlay.remove();
+            }
+        })
+    })
+})
